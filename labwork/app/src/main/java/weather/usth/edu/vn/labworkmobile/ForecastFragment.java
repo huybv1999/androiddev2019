038ff85581630da041ca4a47ce1e84604c1ee2df
DESKTOP-P8RVHUR\huy <huybv1998@gmail.com>
2018-10-15 21:51:11 +0700

    up to labwork 4

--- labwork/app/src/main/java/weather/usth/edu/vn/labworkmobile/ForecastFragment.java
+++ labwork/app/src/main/java/weather/usth/edu/vn/labworkmobile/ForecastFragment.java
@@ -5,7 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class ForecastFragment extends Fragment {
@@ -14,10 +16,22 @@ public class ForecastFragment extends Fragment {
     }
     @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        TextView day = new TextView(getActivity());
+        day.setText("Thursday");
+        ImageView weather = new ImageView(getContext());
+        weather.setImageResource(R.drawable.ic_action_name);
         View v = new View(getContext());
         //View v = super.onCreateView(inflater, container, savedInstanceState);
-        v.setBackgroundColor(0xFF00FF00);
-        return v;
+        //  v.setBackgroundColor(0xFF00FF00);
+        LinearLayout rootViewGroup = new LinearLayout(getActivity());
+        rootViewGroup.setOrientation(LinearLayout.VERTICAL);
+        rootViewGroup.addView(day);
+        rootViewGroup.addView(weather);
+        rootViewGroup.addView(v);
+        return rootViewGroup;
+
+
+
     }
+
 }

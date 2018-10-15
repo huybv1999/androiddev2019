be58c33b3e36af84be227831dea474db9a67f597
DESKTOP-P8RVHUR\huy <huybv1998@gmail.com>
2018-10-15 10:02:06 +0700

    labwork

--- labwork/app/src/main/java/weather/usth/edu/vn/labworkmobile/MainActivity.java
+++ labwork/app/src/main/java/weather/usth/edu/vn/labworkmobile/MainActivity.java
@@ -2,6 +2,7 @@ package weather.usth.edu.vn.labworkmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mtextView = (TextView) findViewById(R.id.text_message);
+        mtextView.setText("Hello World");
     }
 }
